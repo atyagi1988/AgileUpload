feae0daf70afe90b4df7d823a09f5a0194745223
Amar Tyagi <amar.tyagi.cse@gmail.com>
2020-06-12 21:17:56 -0700

    3rd Commit

--- src/main/java/com/intuitive/main/UpdateCodeMain.java
+++ src/main/java/com/intuitive/main/UpdateCodeMain.java
@@ -10,15 +10,16 @@ import com.agile.px.ICustomAction;
 public class UpdateCodeMain implements ICustomAction {
 
 	public ActionResult doAction(IAgileSession sess, INode arg1, IDataObject arg2) {
-		
+		String output=null;
 		String currentUser = null;
 		try {
 			currentUser = sess.getCurrentUser().getName();
+			output= "Current Logged In user is ==>"+currentUser+ " and in PLM";
 		} catch (APIException e) {
 			e.printStackTrace();
 		}
 		
-		return new ActionResult(ActionResult.STRING, "Current Logged In user is ==>"+currentUser);
+		return new ActionResult(ActionResult.STRING, output);
 	}
 
 	
